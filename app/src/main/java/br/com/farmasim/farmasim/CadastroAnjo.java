8422289262e470f7af36d6d24f8f37f740fba2fd
Leandro Schwab Dias Carneiro <schwab_@hotmail.com>
2018-05-08 02:34:20 -0300

    s
    s

---
+++ app/src/main/java/br/com/farmasim/farmasim/CadastroAnjo.java
@@ -0,0 +1,13 @@
+package br.com.farmasim.farmasim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroAnjo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_anjo);
+    }
+}
---
+++ app/src/main/java/br/com/farmasim/farmasim/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.farmasim.farmasim;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void carregarLogin(View view) {
+     Intent intent = new Intent(this,LoginActivity.class);
+     startActivity(intent);
+
+    }
+}
